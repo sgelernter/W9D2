9b9e7ab37507c7b8f8f5e378bd2e7edd86a6d574
sam <s.gelernter@gmail.com>
2021-11-30 15:15:59 -0500

    Begin click event logic

--- browser_tictactoe/src/ttt-view.js
+++ browser_tictactoe/src/ttt-view.js
@@ -1,27 +1,40 @@
 class View {
   constructor(game, el) {
     document.body.appendChild(this.setupBoard());
+    this.game = game;
+    this.bindEvents();
   }
 
   setupBoard() {
     let ul = document.createElement('ul');
     ul.className = 'board';
     
-    for (let i = 1; i <= 9; i++) {
+    for (let i = 0; i < 9; i++) {
       let li = document.createElement('li');
-      li.id = `box ${i}`;
+      li.id = i;
       ul.append(li);
     }
 
     return ul;
   }
-  
 
-  bindEvents() {}
+  bindEvents() {
+    const grid = document.querySelector("ul");
+    grid.addEventListener("click", this.handleClick);
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    const POSITIONS = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]];
+    let pos = POSITIONS[e.target.id];
+    debugger
+    this.game.playMove(pos);
+    this.makeMove(e.target);
+  }
 
-  makeMove(square) {}
+  makeMove(square) {
+    // if something unclicked gets clicked add "clicked-X/O" class 
+    // depending on game.currentPlayer
+  }
 
 }
 
